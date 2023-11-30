6f3a8524ac7f44f32a477f5ede2e9687a3edc2d8
darq <prototypid@gmail.com>
2023-11-30 09:35:39 +0530

    feat: Add breakpoints to layout for responsiveness

--- app/page.js
+++ app/page.js
@@ -2,5 +2,29 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>Content</main>;
+  return (
+    <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
+      tempora esse animi aliquam sit expedita possimus, asperiores quos veniam
+      qui aspernatur obcaecati a ipsa, totam consequuntur eveniet sunt! Natus.
+    </main>
+  );
 }
